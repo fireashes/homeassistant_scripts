36bb78680092912fe270753703e15c159d206de0
Ashes <fireashes@users.noreply.github.com>
2024-02-12 23:27:42 -0600

    adding text output for os update and upgrades

--- ha_utils/update_upgrade_debian.sh
+++ ha_utils/update_upgrade_debian.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-apt update && apt list --upgradable && apt dist-upgrade -y && apt upgrade -y && apt install -f && apt autoremove -y
+apt update | tee apt_update_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
+&& apt list --upgradable | tee apt_list_upgradable_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
+&& apt dist-upgrade -y | tee apt_dist-upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
+&& apt upgrade -y | tee apt_upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
+&& apt install -f | tee apt_install_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
+&& apt autoremove -y | tee apt_autoremove_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt
