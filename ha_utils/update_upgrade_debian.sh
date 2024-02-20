b7c244e3021b18a95c52ae2afe27b1d08396c208
Ashes <fireashes@users.noreply.github.com>
2024-02-20 00:00:53 -0600

    Added the backup location for the output of update upgrade debian

--- ha_utils/update_upgrade_debian.sh
+++ ha_utils/update_upgrade_debian.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-apt update | tee apt_update_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
-&& apt list --upgradable | tee apt_list_upgradable_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
-&& apt dist-upgrade -y | tee apt_dist-upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
-&& apt upgrade -y | tee apt_upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
-&& apt install -f | tee apt_install_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt \
-&& apt autoremove -y | tee apt_autoremove_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BACKUP_DIR=$( realpath "${SCRIPT_DIR}/../ha_backup")
+echo SCRIPT_DIR = "${SCRIPT_DIR}"
+echo BACKUP_DIR = "${BACKUP_DIR}"
+apt update | tee "${BACKUP_DIR}/apt_update_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
+&& apt list --upgradable | tee "${BACKUP_DIR}/apt_list_upgradable_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
+&& apt dist-upgrade -y | tee "${BACKUP_DIR}/apt_dist-upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
+&& apt upgrade -V | tee "${BACKUP_DIR}/apt_upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
+&& apt upgrade -y | tee -a "${BACKUP_DIR}/apt_upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
+&& apt install -f | tee "${BACKUP_DIR}/apt_install_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
+&& apt autoremove -y | tee "${BACKUP_DIR}/apt_autoremove_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt"
