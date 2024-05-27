4098f0475f9949ad8af5c172cd0bbe90a263f441
Ashes <fireashes@users.noreply.github.com>
2024-05-27 15:18:43 -0500

    Removed auto yes. Added date first in the backup file. Formatted the script to have pipe align in 1 column

--- ha_utils/update_upgrade_debian.sh
+++ ha_utils/update_upgrade_debian.sh
@@ -3,10 +3,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BACKUP_DIR=$( realpath "${SCRIPT_DIR}/../ha_backup")
 echo SCRIPT_DIR = "${SCRIPT_DIR}"
 echo BACKUP_DIR = "${BACKUP_DIR}"
-apt update | tee "${BACKUP_DIR}/apt_update_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
-&& apt list --upgradable | tee "${BACKUP_DIR}/apt_list_upgradable_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
-&& apt dist-upgrade -y | tee "${BACKUP_DIR}/apt_dist-upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
-&& apt upgrade -V | tee "${BACKUP_DIR}/apt_upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
-&& apt upgrade -y | tee -a "${BACKUP_DIR}/apt_upgrade_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
-&& apt install -f | tee "${BACKUP_DIR}/apt_install_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt" \
-&& apt autoremove -y | tee "${BACKUP_DIR}/apt_autoremove_`date +%Y.%m.%d.%H.%M.%S.%3N`.txt"
+apt update               | tee "${BACKUP_DIR}/`date +%Y.%m.%d.%H.%M.%S.%3N`_apt_update.txt" \
+&& apt list --upgradable | tee "${BACKUP_DIR}/`date +%Y.%m.%d.%H.%M.%S.%3N`_apt_list_upgradable.txt" \
+&& apt dist-upgrade      | tee "${BACKUP_DIR}/`date +%Y.%m.%d.%H.%M.%S.%3N`_apt_dist-upgrade_y.txt" \
+&& apt upgrade -V        | tee "${BACKUP_DIR}/`date +%Y.%m.%d.%H.%M.%S.%3N`_apt_upgrade_V.txt" \
+&& apt upgrade           | tee "${BACKUP_DIR}/`date +%Y.%m.%d.%H.%M.%S.%3N`_apt_upgrade_y.txt" \
+&& apt install -f        | tee "${BACKUP_DIR}/`date +%Y.%m.%d.%H.%M.%S.%3N`_apt_install_f.txt" \
+&& apt autoremove        | tee "${BACKUP_DIR}/`date +%Y.%m.%d.%H.%M.%S.%3N`_apt_autoremove_y.txt"
+
